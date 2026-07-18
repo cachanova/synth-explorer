99383b5e54da6260850112060b7a4400c792cb79
Leela Pakanati <leela.pakanati@gmail.com>
2026-07-18 01:20:20 -0500

    Persist editor workspace across refreshes

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,14 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './lib/theme.tsx'
+import { loadWorkspace } from './lib/workspaceStorage.ts'
 import { StoreProvider } from './store.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <StoreProvider>
-        <App />
-      </StoreProvider>
-    </ThemeProvider>
-  </StrictMode>,
-)
+async function renderApp() {
+  const initialWorkspace = await loadWorkspace()
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <ThemeProvider>
+        <StoreProvider initialWorkspace={initialWorkspace}>
+          <App />
+        </StoreProvider>
+      </ThemeProvider>
+    </StrictMode>,
+  )
+}
+
+void renderApp()
