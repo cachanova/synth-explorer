1add1942930fa7dffb2c5c4e1afdc485a96e2bdd
Leela Pakanati <leela.pakanati@gmail.com>
2026-07-21 21:08:23 -0500

    Set logo eye to 80 percent

--- web/src/components/BrandMark.tsx
+++ web/src/components/BrandMark.tsx
@@ -26,8 +26,8 @@ export function BrandMark({ className }: BrandMarkProps) {
         <circle strokeWidth="3.2" cx="185" cy="80" r="5" />
         <circle strokeWidth="2.6" cx="194" cy="63" r="3.4" />
       </g>
-      <circle stroke="var(--text)" strokeWidth="3" cx="122" cy="100" r="22.5" />
-      <circle fill="var(--text)" cx="122" cy="100" r="9.75" />
+      <circle stroke="var(--text)" strokeWidth="3.2" cx="122" cy="100" r="24" />
+      <circle fill="var(--text)" cx="122" cy="100" r="10.4" />
     </svg>
   )
 }
